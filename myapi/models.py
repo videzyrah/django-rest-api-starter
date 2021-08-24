02cfca732d48703d939cd5d72d4b764a78e3fc78
Gregory Stull (Student) <gstull1@sundevils.asu.edu>
2021-08-24 07:17:31 -0400

    5 plants

--- myapi/models.py
+++ myapi/models.py
@@ -50,9 +50,9 @@ class Plant(models.Model):
         default='a',
         help_text='help text',
     )
-    image = models.CharField(max_length = 200)
+    image = models.CharField(max_length = 200, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    retailers= models.ManyToManyField(Retailer)
+    retailers= models.ManyToManyField(Retailer, blank=True)
     def __str__(self):
         return self.screen_name
 
